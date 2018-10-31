03e314ed1467bf892b583c80a34bc46878bdb716
yoel human flores <44625743+notianime@users.noreply.github.com>
2018-10-30 22:02:58 -0500

    Add files via upload

---
+++ dev/MyApp/MyApp/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MyApp
+{
+    public class Calculator
+    {
+        public double multiplie(string numero, string numero2)
+        {
+            double number1;
+            double number2;
+
+            try
+            {
+                number1 = Double.Parse(numero2);
+                number2 = Double.Parse(numero2);
+            }
+            catch (FormatException ex) {
+                return -1;
+            }
+            catch (OverflowException ex) {
+                return -2;
+            }
+            catch (ArgumentNullException ex) {
+                return -3;
+            }
+            catch (Exception ex) {
+                return 0;
+            }
+            return number1 * number2;
+        }
+        public double sum(string numero, string numero2)
+        {
+            double number1;
+            double number2;
+            Double.TryParse(numero, out number1);
+
+            if (Double.TryParse(numero2, out number2))
+            {
+                //
+            }
+            else
+            {
+                throw new System.FormatException("Haz ingresado un numero incorrect");
+                // Esto es un error
+                //row new IndexOutOfRangeException();i
+                //throw new ArgumentNullException("Name cannot be null", nameof(value));
+            }
+            
+            return number2 + number1;
+                
+        }
+    }
+}
---
+++ dev/MyApp/MyApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MyApp
+{
+    public class Student
+    {
+        public string code {get; set;}
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+    }
+        
+    class Program
+    {
+        public static void mycars()
+        {
+            // aqui mi codigo de carros
+        }
+        public static void mystudents()
+        {
+            // Aquí está la definición de una colección
+            List<Student> mystudents = new List<Student>();
+
+            Student mystudent = new Student();
+            mystudent.code = "RROJASE";
+
+            Student mystudent2 = new Student();
+            mystudent2.code = "PEPITA2";
+
+            MiEstudiante.Add(mystudent);
+            mystudents.Add(mystudent2);
+            //--------------------------------
+            foreach (Student o in mystudents)
+            {
+                Console.WriteLine(o.code);
+            }
+            //--------------------------------
+            for (int i = 0; i < mystudents.Count; i++)
+            {
+                Console.WriteLine(mystudents[i].code);
+            }
+            //--------------------------------
+
+                Console.WriteLine(String.Format("Instancia Alumno: {0} {1}",
+                    mystudent.code, "otro valor"));
+        }
+        static void Main(string[] args)
+        {
+
+            mystudents();
+            mycars();
+
+            Calculator mycalculator = new Calculator();
+            double result = mycalculator.sum("10", "5");
+            Console.Write(String.Format("..{0}", result));
+            //string a = Console.ReadLine();
+
+            //Console.Write(String.Format("..{0}", a));
+            
+            Console.ReadKey();
+
+        }
+    }
+}
